58c86abad0392ed65eee255b5aabfc19110ef424
vigimus <160164175+vigimus@users.noreply.github.com>
2024-03-26 19:33:05 +0100

    Add files via upload

---
+++ javaScript_Villkor.js
@@ -0,0 +1,11 @@
+//Villkor i Javascript if, else, else if e.t.c
+
+var name = "Jeremia";
+
+if (name === "Jeremia") {
+	alert("Halloj Jeremia!");
+} else if (name === "Anders") {
+	alert("Välkommen Anders!");
+} else {
+	alert("Vem är du?");
+} // Viktigt att inte glömma dem här brackets { } när man programmerar
